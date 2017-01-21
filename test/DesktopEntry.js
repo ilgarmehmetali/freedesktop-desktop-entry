a50accb2c9fbb00f44cd33b0b241a7b32683bd30
Mehmet Ali İLGAR <mehmet.ali@milgar.net>
2017-01-22 01:46:35 +0300

    Added object version of test case

---
+++ test/DesktopEntry.js
@@ -0,0 +1,93 @@
+module.exports = {
+  "Desktop Entry": {
+    "comment": " inlineee",
+    "precedingComment": "\n\n# normal comment\n",
+    "entries": {
+      "Version": {
+        "value": "1.0",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Type": {
+        "value": "Application",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Name": {
+        "value": "Foo Viewer",
+        "comment": " inline comment",
+        "precedingComment": "# precedingComment of an entry"
+      },
+      "Comment": {
+        "value": "The best viewer for Foo objects available!",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "TryExec": {
+        "value": "fooview",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Exec": {
+        "value": "fooview %F",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Icon": {
+        "value": "fooview",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "MimeType": {
+        "value": "image/x-foo",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Actions": {
+        "value": [
+          "Gallery",
+          "Create"
+        ],
+        "comment": "",
+        "precedingComment": ""
+      }
+    }
+  },
+  "Desktop Action Gallery": {
+    "comment": "",
+    "precedingComment": "\n",
+    "entries": {
+      "Exec": {
+        "value": "fooview --gallery",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Name": {
+        "value": "Browse Gallery",
+        "comment": "",
+        "precedingComment": ""
+      }
+    }
+  },
+  "Desktop Action Create": {
+    "comment": "",
+    "precedingComment": "\n",
+    "entries": {
+      "Exec": {
+        "value": "fooview --create-new",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Name": {
+        "value": "Create a new Foo!",
+        "comment": "",
+        "precedingComment": ""
+      },
+      "Icon": {
+        "value": "fooview-new",
+        "comment": "",
+        "precedingComment": ""
+      }
+    }
+  }
+}
